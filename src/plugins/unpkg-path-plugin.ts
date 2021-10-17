c6f756b9e5c6ffd77c32de78383bf3636165f6db
Stefanos <stefanosu422@gmail.com>
2021-10-17 17:54:41 -0400

    refactored build functions in unpkg plugin

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -5,25 +5,26 @@ export const unpkgPathPlugin = () => {
     name: 'unpkg-path-plugin',
     setup(build: ebuild.PluginBuild) {
       //Handle root entry file of index.js
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        if (args.path === 'index.js') {
-          return { path: args.path, namespace: 'a' };
-        }
-        if (args.path.includes('./') || args.path.includes('../')) {
-          return {
-            namespace: 'a',
-            path: new URL(
-              args.path,
-              'https://unpkg.com' + args.resolveDir + '/'
-            ).href,
-          };
-        }
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { path: 'index.js', namespace: 'a' };
+      });
 
+      //Handle relative paths in a module
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
         return {
           namespace: 'a',
-          path: `https://unpkg.com/${args.path}`,
+          path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/')
+            .href,
         };
       });
     },
   };
 };
+//         return {
+//           namespace: 'a',
+//           path: `https://unpkg.com/${args.path}`,
+//         };
+//       });
+//     },
+//   };
+// }
