aa026b837c97e8a4eafec48bda9b1c1c9892a624
Stefanos <stefanosu422@gmail.com>
2021-10-18 17:47:05 -0400

    added try catch block for err msg

--- src/index.tsx
+++ src/index.tsx
@@ -34,6 +34,12 @@ const App = () => {
     });
     // console.log(result);
     setCode(result.outputFiles[0].text);
+
+    try {
+      eval(result.outputFiles[0].text);
+    } catch (err) {
+      alert(err);
+    }
   };
 
   return (
