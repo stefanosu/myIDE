4010e4e74de233498f00750eb51e5f286465a503
Stefanos <stefanosu422@gmail.com>
2021-10-20 20:39:46 -0400

    added srcDoc to iframe

--- src/index.tsx
+++ src/index.tsx
@@ -51,9 +51,12 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
-      <iframe sandbox="allow-same-origin" src="/test.html" />
+      <iframe sandbox="" srcDoc={html} />
     </div>
   );
 };
+
+const html = `<h1> Local HTML doc </h1>
+`;
 
 ReactDOM.render(<App />, document.querySelector('#root'));
