20fc5bdd09d1606789b7688f0bf8a44d9f62818e
Stefanos <stefanosu422@gmail.com>
2021-10-22 19:02:43 -0400

    added console.error to show err

--- src/index.tsx
+++ src/index.tsx
@@ -49,7 +49,8 @@ const App = () => {
             eval(event.data)
           } catch (err) {
             const root = document.querySelector('#root');
-            root.innerHTML = '<div style="color: red"><h4>Runtime Error</h4>' + err + '</div>'
+            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+            console.error(err);
             }
           }, false)
       </script>
