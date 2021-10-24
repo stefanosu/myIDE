caa4f5dd4809d7b3612f19445d922c2b80be176a
Stefanos <stefanosu422@gmail.com>
2021-10-23 20:49:48 -0400

    added monaco editor and added styles

---
+++ src/components/code-editor.tsx
@@ -0,0 +1,8 @@
+import MonacoEditor from '@monaco-editor/react';
+
+import React from 'react';
+
+export const CodeEditor = () => {
+  return <MonacoEditor height="500px" />;
+};
+export default CodeEditor;
